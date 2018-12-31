a4b2a76daaeb658d0390688a4eccb278941c924b
Setuu <setuu@neigepluie.net>
2018-12-31 23:22:35 +0900

    Fix eorzea-sunrise time

--- utils/calc_first_sunrise.sh
+++ utils/calc_first_sunrise.sh
@@ -8,9 +8,12 @@ for m in {0..72}; do
 	check_hour=$(expr $m / 60)
 	check_min=$(expr $m % 60)
 	check_time=$(printf "%02d:%02d" $check_hour $check_min)
-	check_eotime=$(${utils_dir}/../eodate --date="${next_year}/01/01 ${check_time}" | sed -e "s/.* //")
-	if [ "$check_eotime" = "00:00" ]; then
-		break
+	check_eohour=$(LANG=C ${utils_dir}/../eodate --date="${next_year}/01/01 ${check_time}" | sed -e "s/.* //" | sed -e "s/:[0-9][0-9]$//")
+	if [ "$check_eohour" = "06" ]; then
+		m=$(expr $m - 1)
+		result_hour=$(expr $m / 60)
+		result_min=$(expr $m % 60)
+		printf "%02d:%02d\n" $result_hour $result_min
+		exit 0
 	fi
 done
-echo "${check_time}"
