cdd08c446cbe0dbca2e75f3d6e4c3ebcd04ade37
Setuu <setuu@neigepluie.net>
2019-01-01 01:28:22 +0900

    Change calculation method of the first eorzea-sunrise time

--- utils/calc_first_sunrise.sh
+++ utils/calc_first_sunrise.sh
@@ -4,16 +4,19 @@ utils_dir=$(dirname $0)
 
 next_year=$(expr $(date +'%Y') + 1)
 
-for m in {0..72}; do
-	check_hour=$(expr $m / 60)
-	check_min=$(expr $m % 60)
-	check_time=$(printf "%02d:%02d" $check_hour $check_min)
-	check_eohour=$(LANG=C ${utils_dir}/../eodate --date="${next_year}/01/01 ${check_time}" | sed -e "s/.* //" | sed -e "s/:[0-9][0-9]$//")
-	if [ "$check_eohour" = "06" ]; then
-		m=$(expr $m - 1)
-		result_hour=$(expr $m / 60)
-		result_min=$(expr $m % 60)
-		printf "%02d:%02d\n" $result_hour $result_min
-		exit 0
-	fi
-done
+midnight_eotime=$(LANG=C ${utils_dir}/../eodate --date="${next_year}/01/01 00:00:00" | sed -e "s/.* //")
+midnight_eohour=$(echo $midnight_eotime | sed -e "s/:[0-9][0-9]$//")
+midnight_eomin=$(echo $midnight_eotime | sed -e "s/^[0-9][0-9]://")
+
+diff_eohour=$(expr \( 29 - $midnight_eohour \) % 24)
+diff_eomin=$(expr 60 - $midnight_eomin)
+
+diff_earthsec_of_eohour=$(expr $diff_eohour \* 175)
+diff_earthsec_of_eomin=$(expr \( $diff_eomin \* 175 \) / 60)
+diff_earthsec=$(expr $diff_earthsec_of_eohour + $diff_earthsec_of_eomin)
+
+# print results
+echo -n "Earth date: "
+date --date="${next_year}/01/01 00:00:00 $diff_earthsec seconds"
+echo -n "Eorzea date: "
+${utils_dir}/../eodate --date="${next_year}/01/01 00:00:00 $diff_earthsec seconds"
